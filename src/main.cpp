3579cc096c0fb5a36e45e594eeb7a151d0db7dc0
te1ny <teiny.work@gmail.com>
2025-10-08 19:15:19 +0800

    Reconstruct

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <QApplication>
-#include <QMainWindow>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
-    QMainWindow window;
-    window.show();
     return app.exec();
 }
